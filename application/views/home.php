246914b1e805fb30df6a6f07b53500f74219b2af
vic <vik.bobkov@gmail.com>
2014-09-10 03:03:20 -0700

    news section update

--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,14 @@
 <ul>
 	<ul class="column-right">
 		<div class="title1">News</div>
-		<li><b>2014-02-17:</b>
-			The website is up and running.
-			The very 1st tutorial I have written up (ever) is about the correct/elegant way to do <a target="_blank" href="/shenanigans/nested_cssmenu">multi-layered menus</a>.
-			And by that I mean using the least lines of code and resources (CPU/RAM/hard drive space/bandwidth/etc) while utilizing just CSS and plain HTML.
-			As a bonus, it includes instructions on <a target="_blank" href="/shenanigans/nested_cssmenu_mobile">how to make it mobile-friendly</a>.
+		<li><b>2014-09-10:</b>
+			Updated the Yuggoth page.  Added an <a target="_blank" href="/shenanigans/yuggoth_sample">example</a>.
+		</li>
+		<li><b>2014-03-21:</b>
+			One of my most re-usable and complex Javascript/JQuery plugins is now up for download. <a target="_blank" href="/shenanigans/yuggoth">Yuggoth</a>
+		</li>
+		<li><b>2014-02-26:</b>
+			Added <a target="_blank" href="/shenanigans/foodship9">a little demo</a> that showcases a few of the game dynamics (animations/player controls/special effects/physics/etc) that are possible with just Javascript and CSS.
 		</li>
 		<li><b>2014-02-23:</b>
 			The <a target="_blank" href="/shenanigans/datatables_template_generator">dataTables.js template generator</a> is up.
@@ -16,11 +19,11 @@
 			<br />
 			My motivation behind this one is mostly convenience and saving time; as with any really extensive plugin, it can be a real pain to get all the formatting right during each initialization/setup. Therefore, this particular page is going to be a long-term project, not just for others', but also for my own convenience.
 		</li>
-		<li><b>2014-02-26:</b>
-			Added <a target="_blank" href="/shenanigans/foodship9">a little demo</a> that showcases a few of the game dynamics (animations/player controls/special effects/physics/etc) that are possible with just Javascript and CSS.
-		</li>
-		<li><b>2014-03-21:</b>
-			One of my most re-usable and complex Javascript/JQuery plugins is now up for download. <a target="_blank" href="/shenanigans/datatables/yuggoth">Yuggoth</a>
+		<li><b>2014-02-17:</b>
+			The website is up and running.
+			The very 1st tutorial I have written up (ever) is about the correct/elegant way to do <a target="_blank" href="/shenanigans/nested_cssmenu">multi-layered menus</a>.
+			And by that I mean using the least lines of code and resources (CPU/RAM/hard drive space/bandwidth/etc) while utilizing just CSS and plain HTML.
+			As a bonus, it includes instructions on <a target="_blank" href="/shenanigans/nested_cssmenu_mobile">how to make it mobile-friendly</a>.
 		</li>
 	</ul>
 	<ul class="column-center">
@@ -37,6 +40,6 @@
 		The premise behind this is that, for more complex images, if they can be done programmatically via pure HTML/CSS/Javascript, their programmed versions will almost always be significantly less in size than whatever image format they were in to begin with.
 		This is because image formats inherently try to account for each and every single pixel inside the image, while HTML/CSS/Javascript can generalize collections of pixels into much more encompassing rulesets.
 		<br /><br />
-		Of course, <a target="_blank" href="/shenanigans/foodship9">sometimes images are unavoidable</a>, but even then one can easily minimize the space they take up and when they are loaded.
+		Of course, <a target="_blank" href="/shenanigans/foodship9">sometimes</a>, <a target="_blank" href="/shenanigans/yuggoth_sample">images are unavoidable</a>, but even then one can easily minimize the space they take up and when they are loaded.
 	</ul>
 </ul>
