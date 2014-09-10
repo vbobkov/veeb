c9bd6cd57c1fba983dcae57e6c449b89e58b43b2
vic <vik.bobkov@gmail.com>
2014-09-10 01:50:04 -0700

    yuggoth article edits

--- application/views/yuggoth.php
+++ application/views/yuggoth.php
@@ -2,11 +2,12 @@
 To properly use this plugin, extensive knowledge of <a target="_blank" href="http://datatables.net/">dataTables</a> is highly recommended.
 <br />
 Essentially, Yuggoth extends the functionality of <a target="_blank" href="http://datatables.net/">dataTables</a> to include:
-<li>Highly customizable click-and-go table cell editing. Clicking editable cells turns them into input/textarea boxes, navigating away from them automatically saves them to the database.</li>
+<li>Highly customizable click-and-go table cell editing. Clicking editable cells turns them into input/textarea boxes, navigating away from them automatically saves them.</li>
+<li>Every little change is saveable, and by default shows up highlighted in red. Specifying a save URL allows every single table edit to be saved to a back-end database via server-side Ajax calls.</li>
+<li>Customizable <a target="_blank" href="http://redis.io/">Redis</a> support! The redis settings can be customized to post to a URL of your choice every edit/add/delete.</li>
 <li>Customizable column formats/css classes.</li>
 <li>Fully customizable custom commands (redundant redudancy, heh).  Anything from setting certain columns to specific values, to, well. Anything. Includes specifying their CSS classes, their ID names, their location in the HTML DOM tree, etc.</li>
-<li>Mutators. No, it's not just a cool-sounding name for something that already exists. Ever wanted certain database rows and/or table cells to update their value if, say, some other column got updated? Well, that's exactly what mutators do. Fully chainable (column 1 modifies column2, which changes column4, which is a sum of column2 and column3, etc, etc.). Just be careful with infinite loops!</li>
-<li>Every little change is saveable, and by default shows up highlighted in red. Specifying a save URL allows every single table edit to be saved to a back-end database via server-side Ajax calls.</li>
+<li>Mutators. No, it's not just a cool-sounding name. Ever wanted certain database rows and/or table cells to update their value if, say, some other column got updated? Well, that's exactly what mutators do. Fully chainable (column 1 modifies column2, which changes column4, which is a sum of column2 and column3, etc, etc.). Just be careful with infinite loops!</li>
 <li>Image previewer. Hovering over an image preview cell will trigger a chat bubble preview of the image. Can be combined with editable cells.</li>
 <li>Optionally resizable/draggable table contents (requires <a target="_blank" href="https://jqueryui.com/">JQuery UI</a>).</li>
 
@@ -24,5 +25,5 @@ The latest source code for Yuggoth can be found <a target="_blank" href="/assets
 <br />
 Yuggoth is open source under the <a target="_blank" href="http://www.gnu.org/licenses/gpl.html">GPL v3 license</a>.
 <br /><br />
-<a target="_blank" href="/shenanigans/yuggoth_sample">Sample of Yuggoth in action.</a> (2 tables - 50,000 and 25,000 entries)
+<a target="_blank" href="/shenanigans/yuggoth_sample">Sample of Yuggoth in action.</a> (Barebone, no server-side stuff. 2 tables, 50,000 and 25,000 entries, no database saving, redis or mutators.)
 </ul>
