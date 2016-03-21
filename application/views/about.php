7302820bd8c73eea85b13479ca84352ce8d53555
vic <vik.bobkov@gmail.com>
2016-03-20 22:41:09 -0700

    about page tweakage

--- application/views/about.php
+++ application/views/about.php
@@ -1,8 +1,8 @@
 <ul>
 	<div class="title1">Who Am I And What Do I Do?</div>
-	What is up, I am VeeB. My actual name is Victor Bobkov and I am a Developer & System Administrator.
+	My actual name is Victor Bobkov and I am a Developer & System Administrator.
 	<br /><br />
-	Essentially, I am a software developer, gamer and frequenter of many local restaurants.
+	Essentially, I am a software developer, scientist, gamer and frequenter of many local restaurants.
 	<br /><br />
 	I am a firm believer in black magic, because <a target="_blank" href="http://en.wikipedia.org/wiki/Fast_inverse_square_root">it is as real as the rock we consume air on</a>.
 	<br /><br />
