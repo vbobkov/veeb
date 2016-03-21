a6e3567241ddc3b4751b251e0b22a48ba96c69d2
vic <vik.bobkov@gmail.com>
2016-03-20 22:47:56 -0700

    home page tweakage

--- application/views/home.php
+++ application/views/home.php
@@ -30,12 +30,8 @@
 		I am VeeB and this is my personal website.
 		<br /><br />
 		What makes this website unique is that its design is <b>completely free of image data</b>.
-		This includes all conceivable loopholes, such as <a target="_blank" href="http://en.wikipedia.org/wiki/Data_URI_scheme">data URIs</a> and <a target="_blank" href="http://fortawesome.github.io/Font-Awesome/">custom fonts</a>.
-		<br /><br />
-		You're not hallucinating, it's not a trick of the eyes. Well, actually it kinda is.
-		<br /><br />
-		What you see is completely free of any image data, GIF, JPEG, BMP or otherwise.
 		All of the site's design is done via plain HTML and CSS.
+		This includes all conceivable loopholes, such as <a target="_blank" href="http://en.wikipedia.org/wiki/Data_URI_scheme">data URIs</a> and <a target="_blank" href="http://fortawesome.github.io/Font-Awesome/">custom fonts</a>.
 		<br /><br />
 		The premise behind this is that, for more complex images, if they can be done programmatically via pure HTML/CSS/Javascript, their programmed versions will almost always be significantly less in size than whatever image format they were in to begin with.
 		This is because image formats inherently try to account for each and every single pixel inside the image, while HTML/CSS/Javascript can generalize collections of pixels into much more encompassing rulesets.
