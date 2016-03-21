a7f30b0cb9f249194d9aacb0580b741c7eddd4be
vic <vik.bobkov@gmail.com>
2016-03-20 23:55:01 -0700

    about page tweakage

--- application/views/about.php
+++ application/views/about.php
@@ -1,8 +1,10 @@
 <ul>
+	<!--
 	<div class="title1">Who Am I And What Do I Do?</div>
-	My actual name is Victor Bobkov and I am a Developer & System Administrator.
+	-->
+	My actual name is Victor Bobkov and I am primarily a developer & system administrator.  I suppose the more formal term would be Computer Scientist.
 	<br /><br />
-	Essentially, I am a software developer, scientist, gamer and frequenter of many local restaurants.
+	Essentially, a software engineer, gamer and frequenter of many local restaurants.
 	<br /><br />
 	I am a firm believer in black magic, because <a target="_blank" href="http://en.wikipedia.org/wiki/Fast_inverse_square_root">it is as real as the rock we consume air on</a>.
 	<br /><br />
